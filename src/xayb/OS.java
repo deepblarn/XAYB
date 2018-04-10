3d9571863c1dae247dd43ee416dbebd32376762c
jaume5900 <jaume5900@hotmail.es>
2018-04-10 23:22:09 +0200

    OS optimization class

---
+++ src/xayb/OS.java
@@ -0,0 +1,16 @@
+package xayb;
+
+public class OS {
+
+    private static String OS = System.getProperty("os.name").toLowerCase();
+
+    public static void OS(){
+        if (OS.contains("win")){
+            System.setProperty("sun.java2d.transaccel", "True");
+            System.setProperty("sun.java2d.d3d", "True");
+            System.setProperty("sun.java2d.ddforcevram", "True");
+        }else{
+            System.setProperty("sun.java2d.opengl", "True");
+        }
+    }
+}
